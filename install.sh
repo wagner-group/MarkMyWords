f3ecd3af7dd7fe723bc55d2feb7af39512b3cfbd
julien <julien.piet@berkeley.edu>
2024-04-10 13:58:05 -0700

    Updated requirements

--- install.sh
+++ install.sh
@@ -8,11 +8,8 @@ git submodule update
 
 export CUDA 
 
-#python3 -m venv env
-#source env/bin/activate
 pip install --upgrade pip
-
-pip install torch torchvision torchaudio xformers --index-url https://download.pytorch.org/whl/cu118 || exit 1
+pip install torch torchvision torchaudio xformers || exit 1
 pip install --upgrade setuptools
 
 echo "=> Installing cpp-hash..."
@@ -20,17 +17,14 @@ cd submodules/cpp-hash || exit 1
 python setup.py install || exit 1
 cd ../..
 
-# Install specific versions of packages for VLLM compatibility
-pip install lingua-language-detector tiktoken transformers scikit-learn nltk pyinflect accelerate openai textattack pandas dacite dahuffman argostranslate
-
-pip install starlette==0.27.0
-
 echo "=> Installing vllm..."
-cd submodules/vllm || exit 1
-python setup.py install || exit 1
-cd ../..
+pip install submodules/vllm || exit 1
 
-pip install torch torchvision torchaudio xformers --index-url https://download.pytorch.org/whl/cu118 || exit 1
+
+# Install specific versions of packages for VLLM compatibility
+pip install lingua-language-detector tiktoken transformers scikit-learn nltk pyinflect accelerate openai textattack pandas dacite dahuffman argostranslate dill
+
+#pip install starlette==0.27.0
 
 echo "=> Installing watermark-benchmark..."
 python setup.py install || exit 1
